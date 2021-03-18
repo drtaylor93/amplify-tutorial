1f41b649a84074f4dcf2c2d41f0455316681cce4
drtaylor93 <drtaylor.gt@gmail.com>
2021-03-18 19:38:47 -0400

    added part 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dre's AWS Amplify Tutorial
+          Dre's AWS Amplify Tutorial - Part 2
         </p>
         <a
           className="App-link"
